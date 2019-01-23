fea021dc8a164fbe00fbfa1c20d73e30d20f2ad9
Kalaria <kalariaobasi@gmail.com>
2019-01-22 16:00:31 -0800

    Hover images
    created id's for phone images to transform image size upon

--- tunevibe.js
+++ tunevibe.js
@@ -1,14 +1,15 @@
-function deferVideo() {
+// function deferVideo() {
 
     //defer html5 video loading
-  $("video source").each(function() {
-    var sourceFile = $(this).attr("data-src");
-    $(this).attr("src", sourceFile);
-    var video = this.parentElement;
-    video.load();
+  // $("video source").each(function() {
+  //   var sourceFile = $(this).attr("data-src");
+  //   $(this).attr("src", sourceFile);
+  //   var video = this.parentElement;
+  //   video.load();
     // uncomment if video is not autoplay
     //video.play();
-  });
+//   });
 
-}
-window.onload = deferVideo;
+// }
+// window.onload = deferVideo;
+
